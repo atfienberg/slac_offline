6d8ce690ee7f5d8743b9ec493e8cab897cd00398
Aaron Fienberg <fienberg@uw.edu>
2014-04-14 18:05:40 -0700

    removed some unneccesary couts

--- src/exampleDriver.cxx
+++ src/exampleDriver.cxx
@@ -10,20 +10,19 @@ using namespace std;
 int main(){
   new TApplication("app", 0, nullptr);
   TFile* datafile = new TFile("datafiles/exampleDatafile.root");
-  cout << "?" << endl;
+  
   TTree* tree = (TTree*) datafile->Get("t");
   float trace[1024];
   
-  cout << "..." << endl;
+
   tree->SetBranchAddress("trace", trace);
 
   pulseFitter pf((char*)"configs/exampleSipmConfig.json");
   
-  cout << "Huh? " << endl;
+
 
   for(int i = 0; i <tree->GetEntries(); ++i){
     tree->GetEntry(i);
-    cout << i << endl;
     pf.fitSingle(trace);
   }
 
