2573f01dbc2df7fc9ba237a064ecb8084525989c
Aaron Fienberg <fienberg@uw.edu>
2014-07-15 16:45:46 -0700

    fixed copy_configs.py

--- pythonScripts/copy_configs.py
+++ pythonScripts/copy_configs.py
@@ -9,8 +9,8 @@ for i in xrange(4):
         with open("../configs/sipm22.json", "r") as in_file:
              with open("../configs/sipm%i%i.json" % (i+2, j+2), "w") as out_file:
                  for line in in_file:
-                     if "sipm52" in line:
-                         out_file.write(line.replace("sipm52", "sipm%i%i" % (i+2,j+2)))
+                     if "sipm22" in line:
+                         out_file.write(line.replace("sipm22", "sipm%i%i" % (i+2,j+2)))
                      else:
                          out_file.write(line)
 
